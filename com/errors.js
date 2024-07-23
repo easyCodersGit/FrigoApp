e8198c1679bef76b1fd33078bd5566adadbdfe2e
Anna Cuenca <acuencabo@gmail.com>
2024-07-23 22:13:40 +0200

    add errors and validate to com folder

---
+++ com/errors.js
@@ -0,0 +1,82 @@
+class NotFoundError extends Error {
+    constructor(message) {
+
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class SystemError extends Error {
+    constructor(message) {
+
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class ContentError extends Error {
+    constructor(message) {
+
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class DuplicityError extends Error {
+    constructor(message) {
+
+        super(message) // podemos ponerle el message aqui?
+
+        this.name = this.constructor.name
+    }
+}
+
+class AuthenticateError extends Error {
+    constructor(message) {
+
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class CredentialsError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = this.constructor.name
+    }
+}
+
+class TokenError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+
+
+export {  //esto es un bucket
+    NotFoundError,
+    SystemError,
+    ContentError,
+    DuplicityError,
+    AuthenticateError,
+    CredentialsError,
+    TokenError
+}
+
+const errors = { // esto un objeto
+    NotFoundError,
+    SystemError,
+    ContentError,
+    DuplicityError,
+    CredentialsError,
+    TokenError
+}
+
+export default errors
