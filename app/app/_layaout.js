333262313f08cbd05efcfd912dd8a9db4d3df9af
Anna Cuenca <acuencabo@gmail.com>
2024-07-25 21:50:27 +0200

    add app folder and intall react native with expo

---
+++ app/app/_layaout.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import { Slot } from "expo-router";
+
+export default function Layout() {
+    <View>
+        {/* <Text>Hola</Text> */}
+        <Slot />
+    </View>
+}
