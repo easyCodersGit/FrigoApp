3a6011bd43d335a8e80ace9f9a5e5e063a54cfde
Anna Cuenca <acuencabo@gmail.com>
2024-08-08 23:09:09 +0200

    add retrieveFridge logic and test in api #12

--- api/logic/index.js
+++ api/logic/index.js
@@ -1,13 +1,15 @@
-import retrieveUser from "./retrieveUser.js";
-import authenticateUser from "./authenticateUser.js";
+import retrieveUser from "./retrieveUser.js"
+import authenticateUser from "./authenticateUser.js"
 import addFridge from "./addFridge.js"
-import retrieveUserFridges from "./retrieveUserFridges.js";
+import retrieveUserFridges from "./retrieveUserFridges.js"
+import retrieveFridge from "./retrieveFridge.js"
 
 const logic = {
     retrieveUser,
     authenticateUser,
     addFridge,
-    retrieveUserFridges
+    retrieveUserFridges,
+    retrieveFridge
 }
 
 export default logic
