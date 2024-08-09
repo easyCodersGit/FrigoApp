9ae574463a005099dc9b1919c88caff39f0aa4b3
Anna Cuenca <acuencabo@gmail.com>
2024-08-09 18:57:34 +0200

    create addDrawerHandler, sh file and endpoint #8

--- api/logic/index.js
+++ api/logic/index.js
@@ -3,13 +3,15 @@ import authenticateUser from "./authenticateUser.js"
 import addFridge from "./addFridge.js"
 import retrieveUserFridges from "./retrieveUserFridges.js"
 import retrieveFridge from "./retrieveFridge.js"
+import addDrawer from "./addDrawer.js"
 
 const logic = {
     retrieveUser,
     authenticateUser,
     addFridge,
     retrieveUserFridges,
-    retrieveFridge
+    retrieveFridge,
+    addDrawer
 }
 
 export default logic
